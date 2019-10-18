a9cbda539787fc6705eb565e8a58a6cec96c9d55
Arpon Ascension <arponascension9@gmail.com>
2019-10-18 21:20:15 +0600

    initial commit

---
+++ index.php
@@ -0,0 +1,129 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="logo">Arpon</div>
+        <nav>
+            <ul>
+                <Li><a href="#">Home</a></Li>
+                <Li><a href="#">Contact</a></Li>
+                <Li><a href="#">Support</a></Li>
+                <Li><a href="#">About</a></Li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container">
+        <div class="content row">
+            <div class="main-content">
+                <h1 class="recent-post-title">Recent Posts</h1>
+                <div class="post">
+                    <img src="img/profile.png" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h1><a href="">Thne sfnegf  jge tgjdvgg ejv </a></h1>
+                        <i class="far fa-user">
+                            Arpon Ascension
+                        </i>
+                        &nbsp;
+                        <i class="far calender">
+                            Mar 11, 2019
+                        </i><br><br>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque error exercitationem fuga impedit iure laborum possimus quaerat quod. Amet aut esse hic modi temporibus. Adipisci consectetur dolore dolorem nemo nihil......
+                        </p>
+                        <a href="" class="btn read-more">Read Me</a>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="img/profile.png" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h1><a href="">Thne sfnegf  jge tgjdvgg ejv </a></h1>
+                        <i class="far fa-user">
+                            Arpon Ascension
+                        </i>
+                        &nbsp;
+                        <i class="far calender">
+                            Mar 11, 2019
+                        </i><br><br>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque error exercitationem fuga impedit iure laborum possimus quaerat quod. Amet aut esse hic modi temporibus. Adipisci consectetur dolore dolorem nemo nihil......
+                        </p>
+                        <a href="" class="btn read-more">Read Me</a>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="img/profile.png" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h1><a href="">Thne sfnegf  jge tgjdvgg ejv </a></h1>
+                        <i class="far fa-user">
+                            Arpon Ascension
+                        </i>
+                        &nbsp;
+                        <i class="far calender">
+                            Mar 11, 2019
+                        </i><br><br>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque error exercitationem fuga impedit iure laborum possimus quaerat quod. Amet aut esse hic modi temporibus. Adipisci consectetur dolore dolorem nemo nihil......
+                        </p>
+                        <a href="" class="btn read-more">Read Me</a>
+                    </div>
+                </div>
+                <div class="post">
+                    <img src="img/profile.png" alt="" class="post-image">
+                    <div class="post-preview">
+                        <h1><a href="">Thne sfnegf  jge tgjdvgg ejv </a></h1>
+                        <i class="far fa-user">
+                            Arpon Ascension
+                        </i>
+                        &nbsp;
+                        <i class="far calender">
+                            Mar 11, 2019
+                        </i><br><br>
+                        <p class="preview-text">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque error exercitationem fuga impedit iure laborum possimus quaerat quod. Amet aut esse hic modi temporibus. Adipisci consectetur dolore dolorem nemo nihil......
+                        </p>
+                        <a href="" class="btn read-more">Read Me</a>
+                    </div>
+                </div>
+            </div>
+            <div class="sidebar">
+                <div class="section search">
+                    <h1 class="section-title">Search</h1>
+                    <form action="index.php" method="post">
+                        <input type="text" class="text-input" name="search-term" id="" placeholder="Search.....">
+                    </form>
+                </div>
+                <div class="section topics">
+                    <h2 class="section-title">
+                        Topics
+                    </h2>
+                    <ul>
+                        <li><a href="">Poems</a></li>
+                        <li><a href="">Quotes</a></li>
+                        <li><a href="">Fiction</a></li>
+                        <li><a href="">Biography</a></li>
+                        <li><a href="">Motivations</a></li>
+                        <li><a href="">Poems</a></li>
+                        <li><a href="">Quotes</a></li>
+                        <li><a href="">Fiction</a></li>
+                        <li><a href="">Biography</a></li>
+                        <li><a href="">Motivations</a></li>
+
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<script>
+</script>
+</body>
+</html>
